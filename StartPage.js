780588dbd2c54f851de815517e68672002a79ce7
aroe233 <71332469+aroe233@users.noreply.github.com>
2020-10-08 10:30:19 +0100

    Add files via upload
    Updated "Start Page":
    1. Includes an input for a nickname, the option to change the colour of the player, and rotating instructions. 
    2. The page still needs to be linked to the gameplay and the player nickname and chosen colour needs to be stored.

--- StartPage.js
+++ StartPage.js
@@ -1,35 +1,17 @@
- 
+var pics = [
+	"Web Programming/images/orange.jpg",
+	"Web Programming/images/pink.jpg",
+	"Web Programming/images/blue.jpg"
+]
 
-var pics = [ 
+var button2 = document.querySelector("#button2");
+var image = document.querySelector("img");
+var counter = 1;
 
-    "Web Programming/images/orange.jpg", 
-    
-    "Web Programming/images/pink.jpg", 
-    
-    "Web Programming/images/blue.jpg" 
-    
-    ] 
-    
-     
-    
-    var btn = document.querySelector(#btn2); 
-    
-    var image = document.querySelector("img"); 
-    
-    var counter = 1; 
-    
-     
-    
-    btn.addEventListener("click", function(){ 
-    
-    if (counter === 2){ 
-    
-    counter = 0; 
-    
-    } 
-    
-    image.src = pics[counter] 
-    
-    counter = counter +1; 
-    
-    }); 
+button2.addEventListener("click", function(){
+	if (counter === 3){
+		counter = 0;
+	}
+	image.src = pics[counter]
+	counter = counter +1;
+});
