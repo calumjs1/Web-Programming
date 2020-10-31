aedb15e689371319cc8e00ff5351b31baa3c81f5
judeg112 <71281605+judeg112@users.noreply.github.com>
2020-10-31 19:59:28 +0000

    serverside implementation
    added php to retrieve the username and send to the database

--- serverside.php
+++ serverside.php
@@ -4,11 +4,17 @@
 $link = mysqli_connect("hostname", "username", "password", "database"); //server details will be added in the future
 
 
-if(!$link) 
+if(!$link)
 {
     die("Error: could not connect to server " . mysqli_connect_error());
 }
 
+
+$username = $_POST['username'];
+
+
+$sql = 'INSERT INTO users (usrname) VALUES ($username)';
+mysqli_query($link, $sql);
 
 
 ?>
