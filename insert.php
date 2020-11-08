3327190d19b3e5c8095f079ac4d0d20334d13915
judeg112 <71281605+judeg112@users.noreply.github.com>
2020-11-08 18:14:59 +0000

    nickname verification
    checks if a nickname is already taken and sends to the database if it isn't

--- insert.php
+++ insert.php
@@ -4,22 +4,22 @@ include_once 'connect.php';
 
 
 if(isset($_POST['submit'])) {
-  $nickname = $_POST['username'];
-
-  $qry = "INSERT INTO details (nickname)
-  VALUES ('$nickname')";
-
-  mysqli_query($link, $qry);
+    $nickname = $_POST['username'];
+    
+    $usrcheck = "SELECT nickname FROM details WHERE nickname = '$nickname'";
+    $sql = mysqli_query($link, $usrcheck);
 
 
-  mysqli_close($link);
+    if(mysqli_num_rows($sql) >=1) {
+      header("Location: index.php");
+    } else if(mysqli_num_rows($sql) < 1) {
+      $qry = "INSERT INTO details (nickname)
+              VALUES ('$nickname')";
 
+      mysqli_query($link, $qry);
+      header('Location: canvas.html');
 
 }
 
 
-
-
-
-
- ?>
+?>
