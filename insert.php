b4ee5fb466f6cc156955ae44549ce542e31f2c89
judeg112 <71281605+judeg112@users.noreply.github.com>
2020-11-13 20:18:33 +0000

    udpate login verification
    updated login system to include password and send encrypted password to server

--- insert.php
+++ insert.php
@@ -4,20 +4,33 @@ include_once 'connect.php';
 
 
 if(isset($_POST['submit'])) {
-    $nickname = $_POST['username'];
-    
-    $usrcheck = "SELECT nickname FROM details WHERE nickname = '$nickname'";
-    $sql = mysqli_query($link, $usrcheck);
+  $nickname = $_POST['username'];
+  $password = sha1($_POST['password']);
+
+  $qry = "SELECT username FROM user WHERE username = :u AND password_sha1  = :p";
+  $stmt = $pdo->prepare($qry);
+
+  $stmt->execute(array(
+            ":u"=>$username,
+            ":p"=>$password_sha1
+          ));
+  $row = $stmt->fetch();
+
+  if($row) {
+    $_SESSION['signedIn'] = true;
+    $_SESSION['username'] = $username;
+    header("Location: canvas.html");
+
+  } else {
+   $_SESSION['flash_error'] = "Invalid username or password";
+   $_SESSION['signed_in'] = false;
+   $_SESSION['username'] = null;
+   header("Location: /index.php");
+
+  }
 
 
-    if(mysqli_num_rows($sql) >=1) {
-      header("Location: index.php");
-    } else if(mysqli_num_rows($sql) < 1) {
-      $qry = "INSERT INTO details (nickname)
-              VALUES ('$nickname')";
 
-      mysqli_query($link, $qry);
-      header('Location: canvas.html');
 
 }
 
