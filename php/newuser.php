affef91d0bb822b19565c4357a06ee087ecaf037
judeg112 <71281605+judeg112@users.noreply.github.com>
2020-11-16 20:35:24 +0000

    update newuser.php

--- php/newuser.php
+++ php/newuser.php
@@ -6,9 +6,12 @@ if(isset($_POST['submit'])) {
   $user = $_POST['username'];
   $pwd = $_POST['password'];
 
+  if($user and $pwd) {
+    $qry = "INSERT INTO 'user' (username, password_sha1) VALUES ('$user', '$pwd')";
+    mysqli_query($conn, $qry);
 
-  $qry = "INSERT INTO 'user' (username, password_sha1) VALUES ('$user', '$pwd')";
-  
+  }
+
 
 }
 
