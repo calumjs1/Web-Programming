fc538c2c9e8abc036cf66a0975569fbc50240f4b
judeg112 <71281605+judeg112@users.noreply.github.com>
2020-11-17 14:15:32 +0000

    Update newuser.php

--- php/newuser.php
+++ php/newuser.php
@@ -5,7 +5,7 @@ require('connect.php');
 if(isset($_POST['submit'])) {
   $user = $_POST['username'];
   $pwd = $_POST['password'];
-
+  //hash system will be implemented 
   if($user and $pwd) {
     $qry = "INSERT INTO 'user' (username, password_sha1) VALUES ('$user', '$pwd')";
     mysqli_query($conn, $qry);
