50e4178f64977d7f9177bfddacc1fd54c7b690f3
judeg112 <71281605+judeg112@users.noreply.github.com>
2020-11-18 19:26:46 +0000

    Update newuser.php
    password now gets salted and hashed to improve security when sent to the database

--- php/newuser.php
+++ php/newuser.php
@@ -6,14 +6,17 @@ if(isset($_POST['submit'])) {
   $user = $_POST['username'];
   $pwd = $_POST['password'];
   //hash system will be implemented
+  $salt = "@$&%£WRHSFAA%B&@%(^*$%&$^£*&£ad"
+
+  $pwdHash = md5($salt . $pwd);
 
   $usrCheck = "SELECT ' username'FROM 'user' WHERE userName='$user'";
   $qry = mysqli_query($conn, $usrCheck);
 
   if(mysqli_num_rows($qry > 0)) {
-    echo '<p>Sorry, that username is already taken.</p>'; //checks if a username already exists on the database 
+    echo '<p>Sorry, that username is already taken.</p>'; //checks if a username already exists on the database
   } else {
-    $qry = "INSERT INTO 'user' (username, password_sha1) VALUES ('$user', '$pwd')";
+    $qry = "INSERT INTO 'user' (username, password) VALUES ('$user', '$pwdHash')";
     mysqli_query($conn, $qry);
 
   }
