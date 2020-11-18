1c3c64a769474214d1897a6b738fe133ad358c76
judeg112 <71281605+judeg112@users.noreply.github.com>
2020-11-18 19:18:54 +0000

    Update newuser.php

--- php/newuser.php
+++ php/newuser.php
@@ -5,12 +5,20 @@ require('Connect.php');
 if(isset($_POST['submit'])) {
   $user = $_POST['username'];
   $pwd = $_POST['password'];
-  //hash system will be implemented 
-  if($user and $pwd) {
+  //hash system will be implemented
+
+  $usrCheck = "SELECT ' username'FROM 'user' WHERE userName='$user'";
+  $qry = mysqli_query($conn, $usrCheck);
+
+  if(mysqli_num_rows($qry > 0)) {
+    echo '<p>Sorry, that username is already taken.</p>'; //checks if a username already exists on the database 
+  } else {
     $qry = "INSERT INTO 'user' (username, password_sha1) VALUES ('$user', '$pwd')";
     mysqli_query($conn, $qry);
 
   }
+
+
 
 
 }
