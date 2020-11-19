b09f6a8d1c6ee8637eaa5ed449026f0cc5bdda4e
judeg112 <71281605+judeg112@users.noreply.github.com>
2020-11-19 09:22:11 +0000

    Update Connect.php

--- php/Connect.php
+++ php/Connect.php
@@ -1,13 +1,11 @@
 <?php
 
-
-$link = mysqli_connect($DB_CONNECT, $DB_USR, $DB_PWD, $DB_NAME);
-
 $DB_CONNECT = "localhost";
-$DB_USR = "root;"
+$DB_USR = "root";
 $DB_PWD = "";
 $DB_NAME = "user-data";
 
+$link = mysqli_connect($DB_CONNECT, $DB_USR, $DB_PWD, $DB_NAME);
 
 if(!$link)
 {
